94d6f238b69b9ae0eeff311ea7fef2917ee04288
Olivia Kim <kimhaejeong2525@gmail.com>
2022-05-26 22:30:28 -0400

    model index and category done

--- models/index.js
+++ models/index.js
@@ -1,17 +1,27 @@
-// import models
+// import all models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey:'category_id',
+})
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey:'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(Tag,{
+  through: ProductTag,
+  foreignKey:'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsTo(Product,{
+  through: ProductTag,
+  foreignKey:'tag_id'
+});
 module.exports = {
   Product,
   Category,
